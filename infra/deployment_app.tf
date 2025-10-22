d0416f8dce43f961d407a960e682ea80b260153a
Thiago Tierre <thiagotierre.lima@gmail.com>
2025-10-22 11:09:24 -0300

    fix: aplica ajustes em arquivos terraforms

--- infra/deployment_app.tf
+++ infra/deployment_app.tf
@@ -1,7 +1,7 @@
 resource "kubernetes_deployment" "challengeone_app" {
 
   depends_on = [
-    kubernetes_deployment.challengeone_db
+    kubernetes_stateful_set.challengeone_db
   ]
 
   metadata {
@@ -25,9 +25,17 @@ resource "kubernetes_deployment" "challengeone_app" {
       }
 
       spec {
+        # init_container {
+        #   name  = "wait-for-db"
+        #   image = "busybox"
+        #   command = [
+        #     "sh", "-c",
+        #     "echo 'Aguardando banco de dados...' && until nc -z challengeone-db 5432; do echo waiting; sleep 3; done; echo 'Banco disponível!'"
+        #   ]
+        # }
         container {
           name              = "challengeone"
-          image             = "luigigb/challengeone-image:1.0"
+          image             = "thiagotierre/challengeone:1"
           image_pull_policy = "Always"
 
           port {
@@ -42,10 +50,10 @@ resource "kubernetes_deployment" "challengeone_app" {
 
           liveness_probe {
             http_get {
-              path = "/api/actuator/health/liveness"
+              path = "/api/actuator/health"
               port = 8080
             }
-            initial_delay_seconds = 10
+            initial_delay_seconds = 20
             period_seconds        = 10
             timeout_seconds       = 2
             failure_threshold     = 3
@@ -53,10 +61,10 @@ resource "kubernetes_deployment" "challengeone_app" {
 
           readiness_probe {
             http_get {
-              path = "/api/actuator/health/readiness"
+              path = "/api/actuator/health"
               port = 8080
             }
-            initial_delay_seconds = 5
+            initial_delay_seconds = 30
             period_seconds        = 5
             timeout_seconds       = 2
             failure_threshold     = 3
@@ -64,8 +72,8 @@ resource "kubernetes_deployment" "challengeone_app" {
           
           resources {
             requests = {
-              cpu    = "200m"
-              memory = "128Mi"
+              cpu    = "500m"
+              memory = "512Mi"
             }
           #   limits = {
           #     cpu    = "500m"
@@ -76,4 +84,5 @@ resource "kubernetes_deployment" "challengeone_app" {
       }
     }
   }
+  wait_for_rollout = false
 }
