e5a586d402af341cec483ae5663e3d3d3cfe0d5b
Thiago Tierre <thiagotierre.lima@gmail.com>
2026-01-09 22:23:05 -0300

    feat(data-source): implementa dataSource para pegar password em base64

---
+++ infrastructure/src/main/java/com/fiap/security/DataSourceConfig.java
@@ -0,0 +1,28 @@
+package com.fiap.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.util.Base64;
+
+@Configuration
+public class DataSourceConfig {
+
+  @Value("${spring.datasource.url}")
+  private String url;
+
+  @Value("${spring.datasource.username}")
+  private String username;
+
+  @Value("${spring.datasource.password}")
+  private String encodedPassword;
+
+  @Bean
+  public DataSource dataSourceDataBase() {
+    String decodedPassword = new String(Base64.getDecoder().decode(encodedPassword));
+    return DataSourceBuilder.create().url(url).username(username).password(decodedPassword).build();
+  }
+}
